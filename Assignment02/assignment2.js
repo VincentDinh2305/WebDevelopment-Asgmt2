4307fc9f190f1504198bda90db99104a6e781e16
VincentDinh2305 <90953477+VincentDinh2305@users.noreply.github.com>
2022-02-16 06:08:36 -0500

    Add files via upload

---
+++ Assignment02/assignment2.js
@@ -0,0 +1,12 @@
+﻿function abc() {
+    var row = document.getElementById("row").value;
+    var column = document.getElementById("column").value;
+    for (var r = 1; r <= row; r++) {
+        table += '<tr>';
+        for (var c = 1; c <= column; c++) {
+            table += '<td>' + c + '</td>';
+        }
+        table += '</tr>';
+    }
+    document.write('<table border=1>' + table + '</table>');
+}
